05a0c9c640ffc34734a6a9dc074ca67cd51b940e
rikeardi <--set>
2024-11-11 12:57:14 +0200

    Djangokit and Svelte

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django_kit_fos import trigger_pattern
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + trigger_pattern
