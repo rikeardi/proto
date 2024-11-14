bb6bea20f8a36c95e3702e433e3e323997063218
rikeardi <--set>
2024-11-14 21:33:00 +0200

    Postgresql container

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-python manage.py collectstatic --noinput
-
+django-admin compilemessages
+python manage.py makemigrations --noinput
 python manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py createsuperuser --noinput
 
 python manage.py runserver 0.0.0.0:8000
